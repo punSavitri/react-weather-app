7726d77c21f0eda12011a76889a32e83347764c5
punSavitri <punshakshi@gmail.com>
2024-04-03 16:52:40 +0100

    added fontawesome weather icons and add link to Github repository

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
+
 import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather ">
+    <div className="Weather">
       <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
@@ -36,61 +37,58 @@ export default function Weather() {
       <main>
         <div className="weather-info ">
           <div className="weather-info-heading">
-            <h1 className="text-start m-1">Paris</h1>
-            <div className="weather-info-detais">Wednesday 14:0 minutes</div>
-            <div className="weather-info-detais">Sunny</div>
+            <h1 className="text-start">Paris</h1>
+            <div className="weather-info-detais text-start">Wednesday 14:0 minutes</div>
+            <div className="weather-info-detais text-start">Cloudy</div>
           </div>
-          <div className="row ">
+          <div className="row mt-5 ">
             <div className="col-sm-6">
               <div className="d-flex">
-                <div className="weather-icon"><img src="./src/images/cloudy.png" /></div>
+                <div className="weather-icon"><i class="fa-solid fa-cloud"></i></div>
                 <div className="current-temperature">16°C</div>
               </div>
-              <div className="col-sm-6">
-                <div>Particiaption:82%</div>
-                <div>Wind:4km/h</div>
-              </div>
+            </div>
+            <div className="col-sm-6">
+              <div>Particiaption:82%</div>
+              <div>Wind:4km/h</div>
             </div>
           </div>
-
         </div>
         <div className="row">
           <div className="weather-forecast">
             <div className="row">
               <div className="col-2">
                 <div>Fri</div>
-                <div>icon</div>
+                <div><i class="fa-solid fa-sun"></i></div>
                 <div>13°</div>
               </div>
               <div className="col-2">
                 <div>Sat</div>
-                <div>icon</div>
+                <div><i class="fa-solid fa-cloud-bolt"></i></div>
                 <div>13°</div>
               </div>
               <div className="col-2">
                 <div>Sun</div>
-                <div>icon</div>
-                <div>13°</div>
+                <div><i class="fa-solid fa-cloud-sun"></i></div>
+                <div>25°</div>
               </div>
               <div className="col-2">
                 <div>Mon</div>
-                <div>icon</div>
-                <div>13°</div>
+                <div><i class="fa-solid fa-cloud-moon-rain"></i></div>
+                <div>20°</div>
               </div>
               <div className="col-2">
                 <div>Tue</div>
-                <div>icon</div>
-                <div>13°</div>
+                <div><i class="fa-solid fa-cloud-moon"></i></div>
+                <div>11°</div>
               </div>
             </div>
           </div>
         </div>
       </main>
-
-
-
-
-
+      <footer className="mt-5">
+        <small>This project was coded by <a href="https://github.com/punSavitri">Savitri Pun</a> and <a href="https://github.com/punSavitri/react-weather-app.git">is open-sourced on GitHub</a> and <a href="">is hosted on Netlify</a></small>
+      </footer>
     </div>
   )
 }
