e596eaeb7be614b2d2cd5b4e415f9e32a5fe8c75
punSavitri <punshakshi@gmail.com>
2024-04-04 02:10:35 +0100

    import axios into Weather.js to make api call

--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,63 @@
 
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
+  //define state variables
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  function showWeatherData(response) {
+    console.log(response);
+    setWeather({
+      cityName: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //make an api call
+    let apiKey = "5b565b9df0c73c61b9800cce1a0e8af7";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+    console.log(apiUrl);
+    //request axios to get data from api 
+    axios.get(apiUrl).then(showWeatherData);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  //create a form variable
   return (
-    <div className="Weather">
+    <div div className="Weather container" >
       <header>
-        <form className="d-flex mt-4">
-          <input type="search" placeholder="Enter a city.." aria-label="Search" className="form-control mx-2" />
+        <form onSubmit={handleSubmit} className="d-flex mt-4">
+          <input type="search" placeholder="Enter a city.." aria-label="Search" className="form-control mx-2" onChange={updateCity} />
           <button type="submit" className="btn btn-primary">Search</button>
           <button type="search" className="btn btn-success mx-2">Current</button>
         </form>
-
       </header>
       <main>
         <div className="weather-info ">
           <div className="weather-info-heading">
-            <h1 className="text-start">Paris</h1>
+            <h1 className="text-start">{weather.cityName}</h1>
             <div className="weather-info-detais text-start">Wednesday 14:0 minutes</div>
-            <div className="weather-info-detais text-start">Cloudy</div>
+            <div className="weather-info-detais text-start">{weather.description}</div>
           </div>
           <div className="row mt-5 ">
             <div className="col-sm-6">
               <div className="d-flex">
                 <div className="weather-icon"><i class="fa-solid fa-cloud"></i></div>
-                <div className="current-temperature">16°C</div>
+                <div className="current-temperature">{weather.temperature}°C</div>
               </div>
             </div>
             <div className="col-sm-6">
               <div>Particiaption:82%</div>
-              <div>Wind:4km/h</div>
+              <div>Wind:{weather.wind}km/h</div>
             </div>
           </div>
         </div>
@@ -71,3 +99,6 @@ export default function Weather() {
     </div>
   )
 }
+
+
+
